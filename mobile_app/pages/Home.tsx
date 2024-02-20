dcbb460a1b127a937f22d72af388904f758450f8
demich_e <demich_e@etna-alternance.net>
2024-02-20 16:26:10 +0100

    displayation

--- mobile_app/pages/Home.tsx
+++ mobile_app/pages/Home.tsx
@@ -1,24 +1,24 @@
-import { StatusBar } from 'expo-status-bar'
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View, ScrollView } from 'react-native'
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { fetchGames } from '../services/api/games/requests'
 import Card from '../components/List'
+import { useLinkProps } from '@react-navigation/native'
 
 export default function Home() {
   const [games, setGames] = useState([])
   useEffect(() => {
-    fetchGames()
+    fetchGames().then((response) => setGames(response.data))
   }, [])
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Free GG</Text>
-      <View>
+      <ScrollView>
         {games.map((game, index) => (
           <Card key={index} {...game} />
         ))}
-      </View>
+      </ScrollView>
     </View>
   )
 }
--- mobile_app/services/api/games/requests.ts
+++ mobile_app/services/api/games/requests.ts
@@ -2,6 +2,6 @@ import { api } from '../api'
 
 export const fetchGames = async () => {
   return await api.get('/games').then((response) => {
-    return response.data
+    return response
   })
 }
