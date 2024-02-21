83b1a59312f65a0a1938a3e84052e730e5b6bdb2
demich_e <demich_e@etna-alternance.net>
2024-02-21 16:22:27 +0100

    style_settings

--- mobile_app/pages/Settings.tsx
+++ mobile_app/pages/Settings.tsx
@@ -4,21 +4,50 @@ import React from 'react'
 
 export default function Settings() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Settings</Text>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Settings</Text>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.box}>
+          <Text style={styles.plateforme}>PC</Text>
+          <Text style={styles.plateforme}>Browser</Text>
+          <Text style={styles.plateforme}>All</Text>
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'navy',
     alignItems: 'center',
     justifyContent: 'center'
   },
   title: {
     color: 'white',
-    fontSize: 70
+    fontSize: 30,
+    textAlign: 'center',
+    fontWeight: '600',
+    marginBottom: 5
+  },
+  body: {
+    backgroundColor: 'white',
+    height: '100%',
+    width: '100%'
+  },
+  box: {
+    backgroundColor: 'navy',
+    margin: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  plateforme: {
+    color: 'white',
+    paddingHorizontal: 5,
+    fontWeight: '600',
+    fontSize: 20
   }
 })
